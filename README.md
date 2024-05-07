fdcd380e5692282464c932044254b3b41172abef
xmb505 <530060699@qq.com>
2024-05-07 16:58:19 +0800

    修改了一下readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+## 欢迎！！！
+
+这里是新毛学习java代码的存储库，每天学习到的新代码都会存储到这里，相当一一个错题本？
+
+你可以提出其他的相关意见什么的，提交issues什么之类的都可以，新人嘛，代码写的烂很正常对吧qwq
+
+
 ## Getting Started
 
 Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
--- src/com/xmb505/HuoQuWeiShu.java
+++ src/com/xmb505/HuoQuWeiShu.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class HuoQuWeiShu {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scannerInput = new Scanner(System.in);
         System.out.println("请输入三位数整数，我来拆开你的数字！");
-        int input = sc.nextInt();
+        int input = scannerInput.nextInt();
         int ge = (input % 10), shi = (input / 10 % 10), bai = (input / 100 % 10);
         System.out.println("个位数是：" + ge);
         System.out.println("十位数是：" + shi);
